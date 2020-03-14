ad5962b492314b34dc7db587fb94150f5dfaf0af
lrcoder <panzh8266@163.com>
2020-03-14 17:32:31 +0800

    创建recode_mvvm分支

---
+++ app/src/main/java/com/android/music/ViewModels/RootActivityViewModel.java
@@ -0,0 +1,12 @@
+package com.android.music.ViewModels;
+
+/**
+ * @ProjectName: IRMusicPlayer
+ * @Package: com.android.music.ViewModels
+ * @ClassName: RootActivityViewModel
+ * @Description: function description
+ * @Author: 27414
+ * @CreateDate: 2020/3/14
+ */
+public class RootActivityViewModel {
+}
